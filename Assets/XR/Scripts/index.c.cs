ade0c8b3a1cb7217570fdac99c1e6a8b8dda9d5b
Hadad Al Akbar <akbarhadadal@gmail.com>
2023-03-16 21:47:46 +0700

    test commit

--- Assets/XR/Scripts/index.c.cs
+++ Assets/XR/Scripts/index.c.cs
@@ -7,7 +7,9 @@ public class index.c : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Hadad
+        // Bayu
+        // Reza
     }
 
     // Update is called once per frame
